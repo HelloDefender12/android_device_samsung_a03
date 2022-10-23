972bf5d0d35f563f5c107164735b43af6fae428b
HelloDefender12 <theunknownmcpegamer1998@gmail.com>
2022-10-23 01:20:36 -0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/samsung/a03
+DEVICE_PATH := device/samsung/A035f
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
